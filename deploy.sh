e09901c92b28c54faed09d1fdc1b8ab4ef364095
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-06-27 17:18:00 +0530

    Test: push

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 cd ~
 sudo rm -rf aws-community-day-talk
 git clone git@github.com:yashvardhan-kukreja/aws-community-day-talk.git
-cd aws-community-day
+cd aws-community-day-talk
 kubectl apply -f deployment.yaml
