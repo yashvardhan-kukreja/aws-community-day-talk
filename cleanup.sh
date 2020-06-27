4af914888d19daca1d7e38e30e77d8ed9858fcee
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-06-27 17:15:30 +0530

    Test: push

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 docker container stop $(docker container ps -a -q) 2>&1 && echo "All containers stopped" || echo "No containers found to stop"
 docker container rm $(docker container ps -a -q) 2>&1 && echo "All containers removed" || echo "No containers found to remove"
 
