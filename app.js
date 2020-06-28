63bfc309360e4c02ed9a04157e8f33d74b8d86ea
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-06-28 17:48:29 +0530

    Test: push

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require("dotenv").config({path: path.resolve(__dirname) + "/.env"});
 app.get("/", (req, res) => {
     res.json({
         meta: {
-            success: true,
+            success: "somethg string",
             message: "Welcome to the AWS Community Day ANZ - Something new and something cool!"
         }
     });
