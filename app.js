c29a55c50c0a5f08e4208f3c782eb5e38fb18eb0
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-06-28 15:31:18 +0530

    Update: message

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
     res.json({
         meta: {
             success: true,
-            message: "Welcome to the AWS Community Day ANZ"
+            message: "Welcome to the AWS Community Day ANZ - Something new and something cool!"
         }
     });
 });
